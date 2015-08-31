7d4937af40ff8ee956980702d2e6793ca412bb46
Tejesh Guggilla <tejesh.chinnu@gmail.com>
2015-08-31 21:57:09 +0530

    Update Number_divisible_by_11.c

--- Number_divisible_by_11.c
+++ Number_divisible_by_11.c
@@ -3,20 +3,24 @@ void div(int);
 void main() {
 	int i, j, x, y, a;
 	printf("enter the number \n");
-	scanf("%d", a);
+	scanf("%d", &a);
 	div(a);
 
 }
 void div(int a) {
-	int i, j, d, s, b[1000], e = 0, o = 0;
+	int i, j, d, c=0, e = 0, o = 0;
 	i = 0;
 	while (a != 0) {
 		j = a % 10;
-		a = a / 10;
-		if (i % 2 == 0)	e = e + j;
+		c++;
+		if (c % 2 == 0)	e = e + j;
 		else
 		{
 			o = o + j;
 		}
+		a = a / 10;
 	}
+	if ((e - o) == 11 || (o - e) == 11 || (e - o) == 0 || (o - e) == 0)	printf("divisible by 11 \n");
+	else
+		printf("not diviible by 11 \n");
 }
