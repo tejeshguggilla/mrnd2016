b72a6672d7ff8ea3499ca56db5bba76f95dd41ae
Tejesh Guggilla <tejesh.chinnu@gmail.com>
2015-09-01 17:24:43 +0530

    Update Sorted_array_insertion.c

--- Sorted_array_insertion.c
+++ Sorted_array_insertion.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void sort(int[], int, int);
+int sort(int[], int, int);
 void main() {
 	int a[100], x, i, j, n, p;
 	printf("enter the range \n");
@@ -12,42 +12,48 @@ void main() {
 
 	printf("enter the number to be inserted\n");
 	scanf("%d", &x);
-	sort(a, x, n);
-
+	p=sort(a, x, n);
+	if (p == 1) {
+		for (i = 0;i <= n;i++) {
+			printf("%d\t", a[i]);
+		}
+	}
 	getch();
 }
-void  sort(int a[], int x, int n) {
-	int m=0, i;
+int  sort(int a[], int x, int n) {
+	int m = 0, i;
 
 	for (i = 0;i < n;i++) {
 		if (x == a[i]) {
 			n++;
-			for (m = (n - 1);m >(i+1 );m--) {
+			for (m = (n - 1);m >(i + 1);m--) {
 				a[m] = a[m - 1];
 			}
-			a[i+1] = x;
-			for (i = 0;i < n;i++) {
-				printf("%d\t", a[i]);
-			}
+			a[i + 1] = x;
+			return 1;
 		}
 		
 	}
-	for (i = 0;i < n;i++) {
-		if (x < a[0]) {
-
+	
+	if (x < a[0]) {
+		for (i = 0;i < n;i++) {
 			n++;
 			for (m = (n - 1);m > 0;m--) {
 				a[m] = a[m - 1];
 			}
 			a[0] = x;
+			return 1;
 		}
+		
+	}
 
-		if (x > a[n - 1]) {
-			n++;
-
-			a[n - 1] = x;
-		}
-		if (x >a[i] && x < a[i + 1]) {
+	if (x > a[n - 1]) {
+		n++;
+		a[n - 1] = x;
+		return 1;
+	}
+	for (i = 0;i < n;i++) {
+		if (x > a[i] && x < a[i + 1]) {
 			n++;
 
 			for (m = (n - 1);m >(i + 1);m--) {
@@ -55,17 +61,8 @@ void  sort(int a[], int x, int n) {
 			}
 			a[i + 1] = x;
 		}
-		
 	}
-	
-	
-		for (i = 0;i < n;i++) {
-			printf("%d\t", a[i]);
-		}
-	
-	
-	
-
-
-	
+	for (i = 0;i < n;i++) {
+		printf("%d\t", a[i]);
+	}
 }
